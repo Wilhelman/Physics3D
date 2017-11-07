2d25020bd010f598c339d8c5e6f1ab2055fa88fb
Guillermo García Subirana <guillermogarsub@gmail.com>
2017-11-07 10:04:36 +0100

    First push

---
+++ Physics3D/ModuleSceneIntro.cpp
@@ -0,0 +1,55 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+	
+
+	testPlane = new Plane(0, 0, 0, 0);
+
+	testPlane->axis = true;
+
+	// TODO 2: Place the camera one unit up in Y and one unit to the right
+	// experiment with different camera placements, then use LookAt()
+	// to make it look at the center
+	App->camera->Position.y += 2;
+	App->camera->LookAt(testPlane->normal);
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleSceneIntro::Update()
+{
+	// TODO 1: Create a Plane primitive. This uses the plane formula
+	// so you have to express the normal of the plane to create 
+	// a plane centered around 0,0. Make that it draw the axis for reference
+
+	testPlane->Render();
+
+	// TODO 6: Draw a sphere of 0.5f radius around the center
+	// Draw somewhere else a cube and a cylinder in wireframe
+
+	return UPDATE_CONTINUE;
+}
+
